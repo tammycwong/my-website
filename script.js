668b9d5c58a7688d0242281421ef0397eff6951c
Tammy <tammycwong@gmail.com>
2021-03-08 21:52:27 -0500

    linked javascript to images

--- script.js
+++ script.js
@@ -1,2 +1,57 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
+const h= screen.height;
+const w= screen.width;
+
+document.getElementById('enlarge').onclick=function(){
+    window.open('images/bananachips.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge1').onclick=function(){
+    window.open('images/quesadilla.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge2').onclick=function(){
+    window.open('images/ricecookercake.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge3').onclick=function(){
+    window.open('images/chippedtooth.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge4').onclick=function(){
+    window.open('images/finger.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge5').onclick=function(){
+    window.open('images/oven.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge6').onclick=function(){
+    window.open('images/biking.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge7').onclick=function(){
+    window.open('images/workout.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge8').onclick=function(){
+    window.open('images/neckstrain.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge9').onclick=function(){
+    window.open('images/eyebrow.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge10').onclick=function(){
+    window.open('images/poopdeck.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge11').onclick=function(){
+    window.open('images/christmas.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge12').onclick=function(){
+    window.open('images/apple.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge13').onclick=function(){
+    window.open('images/vote.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge14').onclick=function(){
+    window.open('images/bridge.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge15').onclick=function(){
+    window.open('images/museum.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge16').onclick=function(){
+    window.open('images/aquarium.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
+document.getElementById('enlarge17').onclick=function(){
+    window.open('images/hike.jpg', '_blank' , 'width='+w+', height='+h+'');
+}
